e580df4296f4482f2965ef9ff983e63e24440ee0
esdras192005 <esdrasfilho395@gmail.com>
2026-07-11 14:02:49 -0300

    Register BatteryOptimizationPlugin in MainActivity

--- resources/native/MainActivity.java
+++ resources/native/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(RestNotificationPlugin.class);
+        registerPlugin(BatteryOptimizationPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
